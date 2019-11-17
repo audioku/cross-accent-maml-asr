caead836b497799666401dbe90554a6db6b5bf8d
Samuel Cahyawijaya <scahyawijaya@connect.hk.ust>
2019-11-17 15:00:40 +0800

    remove debug

--- trainer/asr/analyzer.py
+++ trainer/asr/analyzer.py
@@ -52,9 +52,8 @@ class Analyzer():
 
                     src_lens += src_len
                     trg_lens += trg_len
-                    if idx == 10:
-                        break
-                df = pd.DataFrame({'src':src_lens, 'trg':trg_lens})
+
+        df = pd.DataFrame({'src':src_lens, 'trg':trg_lens})
                 df.to_csv(manifest, index=False)
 
                 print('{} MAX SRC'.format(manifest, df['src'].max()))
