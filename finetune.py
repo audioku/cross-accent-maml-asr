e3fe103a1751a38398730f3c4b1779a5adc67f12
Genta Indra Winata <gentaindrawinata@gmail.com>
2019-11-26 17:33:26 +0800

    fix finetune code

--- finetune.py
+++ finetune.py
@@ -8,15 +8,16 @@ import os
 import torch
 import torch.nn as nn
 import numpy as np
+import random
 
 from torchsummary import summary
 from torch.autograd import Variable
-from trainer.asr.trainer import Trainer
+from trainer.asr.joint_trainer import JointTrainer
 from utils.data import Vocab
 from utils.data_loader import SpectrogramDataset, LogFBankDataset, AudioDataLoader, BucketingSampler
 from utils.functions import load_meta_model, load_joint_model, init_transformer_model, init_optimizer, compute_num_params, generate_labels
 
-parser = argparse.ArgumentParser(description='Finetuning ASR training')
+parser = argparse.ArgumentParser(description='Transformer ASR meta training')
 parser.add_argument('--model', default='TRFS', type=str, help="")
 parser.add_argument('--name', default='model', help="Name of the model for saving")
 
@@ -24,11 +25,13 @@ parser.add_argument('--train-manifest-list', nargs='+', type=str)
 parser.add_argument('--valid-manifest-list', nargs='+', type=str)
 parser.add_argument('--test-manifest-list', nargs='+', type=str)
 
-parser.add_argument('--training-mode', default='meta', type=str, help='')
+parser.add_argument('--train-partition-list', nargs='+', type=float, default=None)
+parser.add_argument('--training-mode', default='meta')
 
 parser.add_argument('--sample-rate', default=22050, type=int, help='Sample rate')
-parser.add_argument('--batch-size', default=20, type=int, help='Batch size for training')
-parser.add_argument('--num-workers', default=4, type=int, help='Number of workers used in data-loading')
+parser.add_argument('--k-train', default=20, type=int, help='Batch size for training')
+
+parser.add_argument('--num-workers', default=8, type=int, help='Number of workers used in data-loading')
 parser.add_argument('--labels-path', default='labels.json', help='Contains all characters for transcription')
 parser.add_argument('--label-smoothing', default=0.0, type=float, help='Label smoothing')
 parser.add_argument('--window-size', default=.02, type=float, help='Window size for spectrogram in seconds')
@@ -36,7 +39,7 @@ parser.add_argument('--window-stride', default=.01, type=float, help='Window str
 parser.add_argument('--window', default='hamming', help='Window type for spectrogram generation')
 parser.add_argument('--epochs', default=1000, type=int, help='Number of training epochs')
 parser.add_argument('--cuda', dest='cuda', action='store_true', help='Use cuda to train model')
-# parser.add_argument('--lr', '--learning-rate', default=3e-4, type=float, help='initial learning rate')
+
 parser.add_argument('--early-stop', default="loss,10", type=str, help='Early stop (loss,10) or (cer,10)')
 parser.add_argument('--save-every', default=5, type=int, help='Save model every certain number of epochs')
 parser.add_argument('--save-folder', default='models/', help='Location to save epoch models')
@@ -69,15 +72,14 @@ parser.add_argument('--dim-emb', default=512, type=int, help='Embedding dimensio
 parser.add_argument('--src-max-len', default=2500, type=int, help='Source max length')
 parser.add_argument('--tgt-max-len', default=1000, type=int, help='Target max length')
 
+# optimizer
+parser.add_argument('--lr', default=1e-4, type=float, help='lr')
+parser.add_argument('--evaluate-every', default=1000, type=int, help='evaluate every')
+
 # Noam optimizer
 parser.add_argument('--warmup', default=4000, type=int, help='Warmup')
 parser.add_argument('--min-lr', default=1e-5, type=float, help='min lr')
 parser.add_argument('--k-lr', default=1, type=float, help='factor lr')
-
-# SGD optimizer
-parser.add_argument('--lr', default=1e-4, type=float, help='lr')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--lr-anneal', default=1.1, type=float, help='lr anneal')
 
 # Decoder search
 parser.add_argument('--beam-search', action='store_true', help='Beam search')
@@ -93,13 +95,9 @@ parser.add_argument('--prob-weight', default=1.0, type=float, help='Probability 
 parser.add_argument('--loss', type=str, default='ce', help='ce or ctc')
 parser.add_argument('--clip', action='store_true', help="clip")
 parser.add_argument('--max-norm', default=400, type=float, help="max norm for clipping")
-parser.add_argument('--is-accu-loss', action='store_true', help="is accu loss. experimental")
 parser.add_argument('--is-factorized', action='store_true', help="is factorized. experimental")
 parser.add_argument('--r', default=100, type=int, help='rank')
 parser.add_argument('--dropout', default=0.1, type=float, help='Dropout')
-
-# shuffle
-parser.add_argument('--shuffle', action='store_true', help='Shuffle')
 
 # input
 parser.add_argument('--input_type', type=str, default='char', help='char or bpe or ipa')
@@ -108,13 +106,23 @@ parser.add_argument('--input_type', type=str, default='char', help='char or bpe 
 parser.add_argument('--rank', default=10, type=float, help="rank")
 parser.add_argument('--factorize', action='store_true', help='factorize')
 
+# Training config
+parser.add_argument('--copy-grad', action='store_true', help="copy grad for MAML") # Useless
+parser.add_argument('--cpu-state-dict', action='store_true', help='store state dict in cpu')
+
+# Finetune
+parser.add_argument('--finetune', action='store_true', help="") 
+
 torch.manual_seed(123456)
 torch.cuda.manual_seed_all(123456)
-
+np.random.seed(123456)
+random.seed(123456)
 args = parser.parse_args()
 USE_CUDA = args.cuda
 
 if __name__ == '__main__':
+    args.name = "finetune_" + args.name
+
     print("="*50)
     print("THE EXPERIMENT LOG IS SAVED IN: " + "log/" + args.name)
     print("TRAINING MANIFEST: ", args.train_manifest_list)
@@ -123,12 +131,10 @@ if __name__ == '__main__':
     print("INPUT TYPE: ", args.input_type)
     print("="*50)
 
-    assert args.continue_from == ""
-
     if not os.path.exists("./log"): os.mkdir("./log")
     for handler in logging.root.handlers[:]: logging.root.removeHandler(handler)
         
-    logging.basicConfig(filename="log/finetune_" + args.name + ".log", filemode='a+', format='%(asctime)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename="log/" + args.name + ".log", filemode='a+', format='%(asctime)s - %(message)s', level=logging.INFO)
     print("RESUME TRAINING")
     logging.info("RESUME TRAINING")
 
@@ -149,13 +155,14 @@ if __name__ == '__main__':
     for label in labels:
         vocab.add_token(label)
         vocab.add_label(label)
-        
-    if args.feat == "spectrogram":
-        train_data = SpectrogramDataset(vocab, args, audio_conf, manifest_filepath_list=args.train_manifest_list, normalize=True, augment=args.augment, input_type=args.input_type, is_train=True)
-    elif args.feat == "logfbank":
-        train_data = LogFBankDataset(vocab, args, audio_conf, manifest_filepath_list=args.train_manifest_list, normalize=True, augment=args.augment, input_type=args.input_type, is_train=True)
-    train_sampler = BucketingSampler(train_data, batch_size=args.batch_size)
-    train_loader = AudioDataLoader(pad_token_id=0, dataset=train_data, num_workers=args.num_workers, batch_sampler=train_sampler)
+
+    train_data_list = []
+    for i in range(len(args.train_manifest_list)):
+        if args.feat == "spectrogram":
+            train_data = SpectrogramDataset(vocab, args, audio_conf, manifest_filepath_list=args.train_manifest_list, normalize=True, augment=args.augment, input_type=args.input_type, is_train=True, partitions=args.train_partition_list)
+        elif args.feat == "logfbank":
+            train_data = LogFBankDataset(vocab, args, audio_conf, manifest_filepath_list=args.train_manifest_list, normalize=True, augment=args.augment, input_type=args.input_type, is_train=True)
+        train_data_list.append(train_data)
 
     valid_loader_list, test_loader_list = [], []
     for i in range(len(args.valid_manifest_list)):
@@ -163,16 +170,13 @@ if __name__ == '__main__':
             valid_data = SpectrogramDataset(vocab, args, audio_conf, manifest_filepath_list=[args.valid_manifest_list[i]], normalize=True, augment=args.augment, input_type=args.input_type)
         elif args.feat == "logfbank":
             valid_data = LogFBankDataset(vocab, args, audio_conf, manifest_filepath_list=[args.valid_manifest_list[i]], normalize=True, augment=False, input_type=args.input_type)
-        valid_sampler = BucketingSampler(valid_data, batch_size=args.batch_size)
-        valid_loader = AudioDataLoader(pad_token_id=0, dataset=valid_data, num_workers=args.num_workers)
+        valid_sampler = BucketingSampler(valid_data, batch_size=args.k_train)
+        valid_loader = AudioDataLoader(pad_token_id=vocab.PAD_ID, dataset=valid_data, num_workers=args.num_workers)
         valid_loader_list.append(valid_loader)
 
     start_epoch = 0
     metrics = None
     loaded_args = None
-    opt = None
-
-    # load_model
     logging.info("Continue from checkpoint:" + args.continue_from)
     if args.training_mode == "meta":
         model, vocab, _, _, epoch, metrics, loaded_args = load_meta_model(args.continue_from)
@@ -190,5 +194,5 @@ if __name__ == '__main__':
 
     print("Parameters: {}(trainable), {}(non-trainable)".format(compute_num_params(model)[0], compute_num_params(model)[1]))
 
-    trainer = Trainer()
-    trainer.train(model, vocab, train_loader, train_sampler, valid_loader_list, opt, loss_type, start_epoch, num_epochs, args, metrics, early_stop=args.early_stop)
+    trainer = JointTrainer()
+    trainer.train(model, vocab, train_data_list, valid_loader_list, loss_type, start_epoch, num_epochs, args, evaluate_every=args.evaluate_every, last_metrics=metrics, early_stop=args.early_stop, cpu_state_dict=args.cpu_state_dict, is_copy_grad=args.copy_grad)
